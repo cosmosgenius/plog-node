9755ecbb1f300c31ff78c9f01cb994da2e4bcf80
Sharat M R <cosmosgenius@gmail.com>
2013-10-28 03:26:18 +0530

    create post and get api to added logs

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 /*jslint node: true */
 'use strict';
 
-var mongoose = require('mongoose');
+var mongoose    =   require('mongoose'),
+    express     =   require('express');
 
+var Log = mongoose.model('Log',{log:String}),
+    app = express();
+
+app.use(express.logger('dev'));
+app.use(function(req, res, next) {
+    var data = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) {
+        data += chunk;
+    });
+    req.on('end', function() {
+        req.rawBody = data;
+        next();
+    });
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', function(req, res) {
+    mongoose.connect("mongodb://localhost/test");
+    Log.find(function(err,logs){
+        res.json(logs);
+        mongoose.disconnect();
+    });
+});
+
+app.post('/',function(req, res) {
+    mongoose.connect("mongodb://localhost/test");
+    var newlog = new Log({log: req.rawBody});
+    newlog.save(function(err){
+        mongoose.disconnect();
+    });
+    res.send("Log Added");
+});
+app.listen(8000);
