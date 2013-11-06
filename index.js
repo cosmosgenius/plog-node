ced4956816086aa93f6d3107cd9b1e08907b6350
Sharat M R <cosmosgenius@gmail.com>
2013-11-07 02:54:21 +0530

    added configuration

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
 'use strict';
 
 var mongoose    =   require('mongoose'),
-    express     =   require('express');
+    express     =   require('express'),
+    http      =   require('http');
 
-var Log = mongoose.model('Log',{log:String}),
-    app = express();
+try{
+    var config      =   require('./instance/config');
+}catch(err){
+    switch(err.code){
+        case 'MODULE_NOT_FOUND':
+            throw 'Config.js doesn\'t exist instance folder.';
+        default :
+            throw err;
+    }
+}
 
-var MONGOURI = "mongodb://localhost/test";
+var Log     = mongoose.model('Log',{log:String}),
+    app     = express(),
+    server  = http.createServer(app);
+
 app.use(express.logger('dev'));
 app.use(function(req, res, next) {
     var data = '';
@@ -25,19 +37,34 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/', function(req, res) {
-    mongoose.connect(MONGOURI);
     Log.find(function(err,logs){
+        if(err){
+            console.dir(err);
+        }
         res.json(logs);
-        mongoose.disconnect();
     });
 });
 
 app.post('/',function(req, res) {
-    mongoose.connect(MONGOURI);
     var newlog = new Log({log: req.rawBody});
     newlog.save(function(err){
-        mongoose.disconnect();
+        if(err){
+            console.dir(err);
+        }
     });
     res.send("Log Added");
 });
-app.listen(8000);
+
+
+mongoose.connect(config.mongouri);
+
+server.listen( config.port, function(err) {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env );
+});
+
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected on app termination');
+    process.exit(0);
+  });
+});
---
+++ instance/sample_config.js
@@ -0,0 +1,9 @@
+/*jslint node: true */
+var config = {};
+
+
+config.mongouri = 'mongodb://localhost:27017/log';
+
+config.port = 8000;
+
+module.exports = config;
