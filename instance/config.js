ba97c4ac7b95ed3a85a921b313aacf4365bc80e1
Sharat M R <cosmosgenius@gmail.com>
2013-11-24 23:56:23 +0530

    added test env

--- instance/config.js
+++ instance/config.js
@@ -5,8 +5,9 @@ var config = {};
 config.mongouri = 'mongodb://localhost:27017/plog';
 
 config.port = 8000;
+config.env = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === 'test') {
+if (config.env === 'test') {
     config.mongouri = 'mongodb://localhost:27017/plog-test';
     config.port = 8500;
 }
