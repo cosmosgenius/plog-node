752f3120427dd93ab671d3f6a22fc4fc29122cae
Sharat M R <cosmosgenius@gmail.com>
2013-12-07 14:58:47 +0530

    added debug in dev options

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function(grunt) {
             },
             dev: {
                 options: {
-                    script: 'index.js'
+                    script: 'index.js',
+                    debug: true
                 }
             },
             prod: {
