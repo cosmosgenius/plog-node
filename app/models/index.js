eaf888d0fb6d0c389d4410fc8fca30d892c0881d
Sharat M R <cosmosgenius@gmail.com>
2014-04-20 05:14:39 +0530

    corrected instance path

--- app/models/index.js
+++ app/models/index.js
@@ -1,6 +1,6 @@
 /*jslint node: true */
 var mongoose = require('mongoose');
-var config  =   require('../instance/config');
+var config  =   require('../../instance/config');
 
 mongoose.connect(config.mongouri);
 module.exports.Log = require('./Log');
