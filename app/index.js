563425eb018f40b8d5a0ec20453e4a93918248fd
Sharat M R <cosmosgenius@gmail.com>
2014-08-25 00:05:40 +0530

    moved some code from app

--- app/index.js
+++ app/index.js
@@ -1,25 +1,12 @@
 /*jslint node: true */
 'use strict';
 
-var express         = require('express'),
-    mongoose        = require('mongoose'),
-    logger          = require('morgan'),
-    responseTime    = require('response-time'),
+var express         = require('express'),    
     logRouter       = require('./routes/log'),
     userRouter      = require('./routes/user');
 
-var app             = express(),
-    env             = process.env.NODE_ENV || 'development';
+var app             = express();
 
-if('production' === env) {
-    app.use(logger());
-}
-
-if('development' === env) {
-    mongoose.set('debug', true);
-    app.use(logger('dev'));
-    app.use(responseTime());
-}
 
 app.use('/users', userRouter);
 app.use('/',logRouter);
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 'use strict';
 
-var mongoose    =   require('mongoose'),
-    http        =   require('http');
+var mongoose        =   require('mongoose'),
+    http            =   require('http'),
+    logger          =   require('morgan'),
+    responseTime    =   require('response-time'),
+    config          =   require('config');
 
+var app             =   require('./app'),   
+    server          =   http.createServer(app),
+    env             =   process.env.NODE_ENV || 'development';
 
-var config      =   require('config'),
-    app         =   require('./app'),
-    server      =   http.createServer(app);
+if('production' === env) {
+    app.use(logger());
+}
 
-
+if('development' === env) {
+    mongoose.set('debug', true);
+    app.use(logger('dev'));
+    app.use(responseTime());
+}
 
 server.listen(config.port, function(err) {
     if(err){
