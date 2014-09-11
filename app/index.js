a9ef6b49582d25ad8d1f5e1be01ebfc921b13cfa
Sharat M R <cosmosgenius@gmail.com>
2014-09-12 00:10:18 +0530

    moved to double quotes

--- app/index.js
+++ app/index.js
@@ -1,14 +1,13 @@
-/*jslint node: true */
-'use strict';
+"use strict";
 
-var express         = require('express'),    
-    logRouter       = require('./routes/log'),
-    userRouter      = require('./routes/user');
+var express         = require("express"),    
+    logRouter       = require("./routes/log"),
+    userRouter      = require("./routes/user");
 
 var app             = express();
 
 
-app.use('/users', userRouter);
-app.use('/',logRouter);
+app.use("/users", userRouter);
+app.use("/",logRouter);
 
 module.exports = app;
