1ccc51c7ec177f3f4bcc9c88bb4cf20577093ae0
Sharat M R <cosmosgenius@gmail.com>
2014-10-02 16:37:07 +0530

    Added cors dep

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 "use strict";
 
-var express         = require("express"),    
+var express         = require("express"),
+    cors            = require("cors"),   
     logRouter       = require("./routes/log"),
     userRouter      = require("./routes/user");
 
 var app             = express();
 
-
+app.use(cors());
 app.use("/users", userRouter);
 app.use("/",logRouter);
 
