8980644e1757c78fb60405560b81d42b97e8108a
Sharat M R <cosmosgenius@gmail.com>
2014-10-15 20:14:44 +0530

    added comments

--- app/controllers/log.controller.js
+++ app/controllers/log.controller.js
@@ -1,6 +1,13 @@
 "use strict";
-var Log = require("../models/log.model");
+var Log = require("../models/log.model"),
+    utils = require("../utils");
 
+/**
+ * Get the array of logs from db
+ * @param  {number}   limit  The limit to apply when fetching from db (default: config.pageLimit)
+ * @param  {number}   pageNo pagination value
+ * @param  {Function} cb     callback function
+ */
 exports.getLogs = function getLogs (limit, pageNo, cb) {
     if(typeof limit === "function") {
         cb = limit;
@@ -16,6 +23,11 @@ exports.getLogs = function getLogs (limit, pageNo, cb) {
         .exec(cb);
 };
 
+/**
+ * Create a new log
+ * @param  {object}   log An object with log string
+ * @param  {Function} cb  callback function called with the newly create log
+ */
 exports.createNewLog = function createNewLog(log, cb) {
     var newlog = new Log(log);
     newlog.save(function(err, log) {
@@ -24,8 +36,43 @@ exports.createNewLog = function createNewLog(log, cb) {
         }
 
         cb(null,{
+            id: log._id,
             log: log.log,
             created_at: log.created_at
         });
     }); 
+};
+
+/**
+ * Delete the log with the given id
+ * @param  {string}   logid id of the log to be deleted
+ * @param  {Function} cb    callback function
+ */
+exports.deleteLog = function deleteLog(logid, cb) {
+    Log.findById(logid, function (err, log) {
+
+    });
+};
+
+/**
+ * Get a log with the given id value
+ * @param  {string}   logid id of the log to be retrived
+ * @param  {Function} cb    callback function called with the retrived log
+ */
+exports.getLog = function getLog(logid, cb) {
+    Log.findById(logid, function (err, log) {
+        var ret;
+
+        if(log) {
+            ret = {
+                id: log._id,
+                log: log.log,
+                created_at: log.created_at
+            };
+        } else {
+            ret = utils.getErrorMessage(404);
+        }
+
+        return cb(null,ret);
+    });
 };
