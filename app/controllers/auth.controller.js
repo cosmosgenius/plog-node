05370c7dd8ee425bb45744fd2d5589e65310ac22
Sharat M R <cosmosgenius@gmail.com>
2014-10-16 22:34:58 +0530

    add auth code to read ACCESS_TOKEN value

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 "use strict";
-var config = require("config");
-
 /**
  * Check is the given token is authorized
  * @param  {string}  token Auth token
  * @return {Boolean}       
  */
 exports.isAuthorized = function isAuthorized(token) {
-    return true;
+    if(token === process.env.ACCESS_TOKEN) {
+        return true;
+    }
+    return false;
 };
--- test/controllers/auth.controller.test.js
+++ test/controllers/auth.controller.test.js
@@ -1,14 +1,23 @@
-/*global describe, it */
+/*jshint expr: true*/
+/*global describe, it, before */
 "use strict";
-
-var config = require("config"),
-    should = require("should");
 
 var authController   = require("../../app/controllers/auth.controller");
 
 describe("Auth Controller", function(){
     describe("isAuthorized function", function(){
-        it("should return true for valid auth token");
-        it("should return false for invalid auth tokern");
+        before(function(){
+            process.env.ACCESS_TOKEN = "5TVEDN34KSU1nsQEf8l5I4X0e981d749";
+        });
+        it("should return true for valid auth token",function(){
+            authController
+                .isAuthorized("5TVEDN34KSU1nsQEf8l5I4X0e981d749")
+                .should.be.true;
+        });
+        it("should return false for invalid auth tokern", function(){
+            authController
+                .isAuthorized("invalid")
+                .should.be.false;
+        });
     });
 });
