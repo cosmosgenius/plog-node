f387dc8908d194df11220693b54621da069a8883
Sharat M R <cosmosgenius@gmail.com>
2015-09-28 00:05:59 +0530

    added initial state

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let gulp = require('gulp');
+let plumber = require('gulp-plumber');
+let mocha = require('gulp-mocha');
+let eslint = require('gulp-eslint');
+let istanbul = require('gulp-istanbul');
+let complexity = require('gulp-complexity');
+let gls = require('gulp-live-server');
+
+
+gulp.task('pre-unit', () => {
+  return gulp.src('apps/**/*.js')
+    .pipe(istanbul({includeUntested: true}))
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('unit', ['pre-unit'] ,() => {
+    return gulp.src('test/**/*.js')
+        .pipe(mocha())
+        .pipe(istanbul.writeReports())
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
+});
+
+gulp.task('test', ['unit']);
+gulp.task('default', []);
