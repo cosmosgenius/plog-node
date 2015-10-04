f906f0ed903029fa6796513107deb901516adcef
Sharat M R <cosmosgenius@gmail.com>
2015-10-05 04:06:29 +0530

    added apps logger

--- apps/index.js
+++ apps/index.js
@@ -2,8 +2,9 @@
 
 let express = require('express');
 let cors = require('cors');
-
+let log = require('../lib/logger').createLogger('apps');
 let app = express();
+
 app.use(cors());
 
 module.exports = app;
